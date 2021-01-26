c9826ef0d6198fba0519bf094057ef6d7a311cd6
amedprime-nakatolab <nakatolab.amedprime@gmail.com>
2021-01-26 14:11:48 +0900

    Remodel pages by Pelican

---
+++ publishconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://amedprime-nakatolab.github.io/'
+RELATIVE_URLS = False
+
+
+DELETE_OUTPUT_DIRECTORY = True
+
+#----------------#
+# Page Hierarchy #
+# PAGE_URL = '{slug}/'
+# PAGE_SAVE_AS = '{slug}/index.html'
+# SLUGIFY_SOURCE = 'basename'
+
+# Following items are often useful when publishing
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
